5f1d12bef73934f4e60c9d3cbfddc7cddfe022f3
Gabriel Sacramento <gabrielsacrame@outlook.com>
2024-03-13 12:08:06 -0300

    add commit

---
+++ WebRADOC_MVC/Data/BancoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebRADOC_MVC.Models;
+
+
+
+namespace WebRADOC_MVC.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext (DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UsuariosModel> Usuarios { get; set; }
+
+    }
+}
---
+++ WebRADOC_MVC/Models/UsuariosModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebRADOC_MVC.Models
+{
+    public class UsuariosModel
+    {
+        public int Id { get; set;}
+        public string Nome { get; set;}
+        public string Email { get; set; }
+        public string Disciplina { get; set; }
+    }
+}
