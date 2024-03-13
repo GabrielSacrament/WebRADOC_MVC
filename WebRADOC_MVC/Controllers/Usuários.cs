271ff2b6f1a657e5cc2171fccb013a46aa4baf70
Gabriel Sacramento <gabrielsacrame@outlook.com>
2024-03-12 21:40:51 -0300

    Add project files.

---
+++ WebRADOC_MVC/Controllers/Usuários.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebRADOC_MVC.Controllers
+{
+    public class Usuários : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Excluir()
+        {
+            return View();
+        }
+    }
+}
